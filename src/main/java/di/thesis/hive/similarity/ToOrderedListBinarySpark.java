ac7db45aca08079e961a1a2e380be00921c15e1b
petrospgithub <petrakisp13@gmail.com>
2019-02-06 16:28:38 +0200

    bug

--- src/main/java/di/thesis/hive/similarity/ToOrderedListBinarySpark.java
+++ src/main/java/di/thesis/hive/similarity/ToOrderedListBinarySpark.java
@@ -520,8 +520,8 @@ public class ToOrderedListBinarySpark extends AbstractGenericUDAFResolver {
 
             ArrayList tempB_result= ((ArrayList)tuples._4()[0]);
 
-          //  if (true)
-            //    throw new UDFArgumentException(tempB_result.size()+"~~~~~~~~"+((ArrayList)(tempB_result.get(0))).size()+"~~~~~~~~~~~~~~~~~~~~~ "+((ArrayList)(tempB_result.get(0))).get(0)+" ~~~~~~~~~~~~~~~~~~~~~~~~");
+          if (true)
+            throw new UDFArgumentException(tempB_result.size()+"~~~~~~~~"+((ArrayList)(tempB_result.get(0))).size()+"~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~");
 
             Object[] trajB_ret= new Object[size];
 
