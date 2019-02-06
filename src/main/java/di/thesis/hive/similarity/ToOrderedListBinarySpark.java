9848349fd7a45fa8e817d4f2c1ad3d5723ccbe06
petrospgithub <petrakisp13@gmail.com>
2019-02-06 14:12:52 +0200

    bug

--- src/main/java/di/thesis/hive/similarity/ToOrderedListBinarySpark.java
+++ src/main/java/di/thesis/hive/similarity/ToOrderedListBinarySpark.java
@@ -447,19 +447,19 @@ public class ToOrderedListBinarySpark extends AbstractGenericUDAFResolver {
             final List<Object> trajBList = new ArrayList<Object>();
 
             //check!
-            try {
+      //      try {
                 for (int i = 0, n = trajBListRaw.size(); i < n; i++) {
                     trajBList.add(ObjectInspectorUtils.copyToStandardObject(trajBListRaw.get(i),
                             PrimitiveObjectInspectorFactory.writableBinaryObjectInspector));
-                }
-            } catch (ClassCastException e) {
+             }
+           /* } catch (ClassCastException e) {
 
                 for (int i = 0, n = trajBListRaw.size(); i < n; i++) {
 
                     trajBList.add(trajBListRaw.get(i));
                 }
            }
-
+*/
             myagg.merge(keyList, valueList, trajAListObj, trajBList);
         }
 
@@ -516,7 +516,7 @@ public class ToOrderedListBinarySpark extends AbstractGenericUDAFResolver {
             ArrayList tempB_result= ((ArrayList)tuples._4()[0]);
 
             if (true)
-                throw new UDFArgumentException("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ "+tempB_result.get(0).getClass()+" ~~~~~~~~~~~~~~~~~~~~~~~~");
+                throw new UDFArgumentException("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ "+((ArrayList)tempB_result.get(0)).get(0).getClass()+" ~~~~~~~~~~~~~~~~~~~~~~~~");
 
             Object[] trajB_ret= new Object[tempB_result.size()];
 
